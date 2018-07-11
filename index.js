cb77515563d6cbcab48501ec56b36b9d885a16b6
rlynjb <rlynjb@users.noreply.github.com>
2018-07-11 13:25:41 -0700

    test async

--- index.js
+++ index.js
@@ -50,25 +50,38 @@ const typeDefs = gql`
 */
 let nearbySearchApi = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.057726,-115.237187&keyword=bar,bars&type=bar&radius=80000&maxprice=2&key=AIzaSyBd0gI0OszcB1VkKFSD0jLbqKleC98N5tY'
 
+let photoApi = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyBd0gI0OszcB1VkKFSD0jLbqKleC98N5tY'
+
 const resolvers = {
   Query: {
-    places: () => {
+    places: async () => {
       let finalData = []
+      
+      const res = await fetch(nearbySearchApi)
+      const json = await res.json()
 
       // use async await instead
-      return fetch( nearbySearchApi )
-        .then(res => res.json())
-        .then(json => {
+      //return fetch( nearbySearchApi )
+        //.then(res => res.json())
+        //.then(json => {
           if (json.status != 'OK') return false
 
           json.results.forEach( i => {
-            finalData.push({ title: i.name })
+            // get photos
+            //i.photos.forEach( p => {
+              // do fetch, p.photo_reference
+            //})
+
+            finalData.push({
+              title: i.name,
+              price: i.price,
+              category: i.types,
+              location: i.vicinity
+            })
           })
-        })
-        .then(() => {
-          console.log(finalData)
+          
           return finalData
-        })
+        //})
     },
 
     books: () => {
