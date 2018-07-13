8809d0d1460849c2d377df08d0a0093e3ed486cc
rlynjb <rlynjb@users.noreply.github.com>
2018-07-13 14:16:25 -0700

    fix async function

--- resolvers.js
+++ resolvers.js
@@ -8,7 +8,7 @@ let nearbySearchApi = 'https://maps.googleapis.com/maps/api/place/nearbysearch/j
 let photoApi = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyBd0gI0OszcB1VkKFSD0jLbqKleC98N5tY'
 
 const api = {
-  get: (v) => {
+  get: async (v) => {
     const res = await fetch(v)
     const json = await res.json()
     if (json.status != 'OK') return false
@@ -19,7 +19,7 @@ const api = {
 
 const resolvers = {
   Query: {
-    places: async () => {
+    places: () => {
       const val = api.get(nearbySearchApi)
 
       let finalData = []
