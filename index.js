9b808563f9d21d4448a246ccc361b4362236b216
rlynjb <rlynjb@users.noreply.github.com>
2018-07-13 12:31:10 -0700

    debug

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const { ApolloServer } = require('apollo-server');
-const { typeDefs } = require('./schema.js');
+const { ApolloServer, gql } = require('apollo-server');
+//const { typeDefs } = require('./schema.js');
 const { resolvers } = require('./resolvers.js');
+
+const typeDefs = gql`
+  type Place {
+    id: String,
+    title: String,
+    images: [String],
+    price: Int,
+    open_hours: [OpenHours],
+    category: [String],
+    description: String,
+    location: Location,
+    website: String
+  }
+  
+  type OpenHours {
+    date: String,
+    time: [Time]
+  }
+  
+  type Time {
+    start: String,
+    end: String
+  }
+  
+  type Location {
+    address: String,
+    zipcode: String,
+    city: String,
+    state: String,
+    country: String
+  }
+
+  type Query {
+    books: [Book],
+    places: [Place]
+  }
+`;
 
 const server = new ApolloServer({ 
   typeDefs, 
