fc920090868ccea6269a613d58d6f11f4c30b892
rlynjb <rlynjb@users.noreply.github.com>
2018-07-15 22:12:36 -0700

    test

--- resolvers.js
+++ resolvers.js
@@ -27,9 +27,9 @@ const resolvers = {
       let v =[]
       for (let i = 0; i < place.photos.length; i++) {
         const b = await fetch(photoApi + place.photos[i].photo_reference)
-        const res = await b.json()
-        v.push(res)
+        v.push(b)
       }
+      console.log(v)
       return v
     }
   },
