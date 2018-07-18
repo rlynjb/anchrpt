685ef19f606938758cc932852e8d21cbd09dc430
rlynjb <rlynjb@users.noreply.github.com>
2018-07-18 12:40:35 -0700

    trst

--- resolvers.js
+++ resolvers.js
@@ -28,7 +28,7 @@ const resolvers = {
       for (let i = 0; i < place.photos.length; i++) {
         let b = await fetch(photoApi + place.photos[i].photo_reference)
         let vc = await b.headers.get('Content-Type')
-        let bodyimg = await b.body
+        let bodyimg = await b.json()
 
         //let imgdata = "data:" + vc + ";base64," + new Buffer(bodyimg).toString('base64')
         
