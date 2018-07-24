00cdfffce540d179ff9a8013dbcadf30a6f4a5d2
rlynjb <rlynjb@users.noreply.github.com>
2018-07-24 13:35:09 -0700

    test

--- resolvers.js
+++ resolvers.js
@@ -12,7 +12,7 @@ let placeDetail = 'https://maps.googleapis.com/maps/api/place/details/json?key='
 
 let api = {
   nextPageToken: null,
-  get: async (v) => {
+  get: async (v) {
     const res = await fetch(v)
     const json = await res.json()
     if (json.status != 'OK') return false
